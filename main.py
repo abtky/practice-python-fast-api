c1d562b15ae7582e181cb9f65470a11d55e939e0
Takuya Abe <takuya.abe@linecorp.com>
2022-02-22 10:54:17 +0900

    feat: Enable query parameter

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+fake_item_db = [
+    {
+        "id": 1,
+        "name": "Foo"
+    },
+    {
+        "id": 2,
+        "name": "Bar"
+    },
+    {
+        "id": 3,
+        "name": "Baz"
+    },
+]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World"}   
 
-@app.get("/items/{item_id}")
+@app.get("/items/")
 async def read_item(item_id: int):
-    return { "item_id": item_id }
+    elm = filter(lambda x: x['id'] == item_id, fake_item_db)
+
+    return elm
