888165e2e641d3a287f5d1eb5040aa33e4bb59e7
takuya.abe <takuya.abe@linecorp.com>
2022-07-30 20:06:19 +0900

    feat: Add a datetime column

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 
 from .database import Base
 
@@ -6,3 +6,4 @@ class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
     label = Column(String, index=True)
+    created_at = Column(DateTime)
